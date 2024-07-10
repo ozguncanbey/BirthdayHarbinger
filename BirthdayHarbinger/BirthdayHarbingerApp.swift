8261ed629c239d4ec42b4bee04ff169f713b8c99
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2024-07-10 13:35:51 +0300

    Initial Commit

---
+++ BirthdayHarbinger/BirthdayHarbingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayHarbingerApp.swift
+//  BirthdayHarbinger
+//
+//  Created by Özgün Can Beydili on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayHarbingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
