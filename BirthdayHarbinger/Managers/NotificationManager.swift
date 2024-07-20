c5c84ff297fd61250ec63c3567be1a0d39496f46
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2024-07-20 18:12:57 +0300

    fix notification bugs

--- BirthdayHarbinger/Managers/NotificationManager.swift
+++ BirthdayHarbinger/Managers/NotificationManager.swift
@@ -24,8 +24,8 @@ final class NotificationManager {
         
         if reminder != .none {
             let content = UNMutableNotificationContent()
-            content.title = "\(person.name)'s birthday is coming..."
-            content.body = "Don't forget to prepare"
+            content.title = "\(person.name)" + NSLocalizedString("faNotTitle", comment: "")
+            content.body = NSLocalizedString("faNotMessage", comment: "")
             content.sound = .default
             
             let calendar = Calendar.current
@@ -62,19 +62,17 @@ final class NotificationManager {
             triggerDateComponents.minute = minute
             
             let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDateComponents, repeats: true)
-            let request = UNNotificationRequest(identifier: person.id.uuidString, content: content, trigger: trigger)
+            let request = UNNotificationRequest(identifier: firstAlertIdentifier, content: content, trigger: trigger)
             
-            UNUserNotificationCenter.current().add(request) { error in
-                if let error = error {
-                    print("fa Error adding notification: \(error)")
-                }
-            }
+            UNUserNotificationCenter.current().add(request)
         } else {
             UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [firstAlertIdentifier])
         }
     }
     
     func scheduleSecondAlertNotification(for person: Personn, hour: Int, minute: Int) {
+        let secondAlertIdentifier = "secondAlert-" + person.id.uuidString
+        
         let content = UNMutableNotificationContent()
         content.title = NSLocalizedString("today", comment: "") + "\(person.name)" + NSLocalizedString("notificationTitle", comment: "")
         content.body = NSLocalizedString("notificationMessage", comment: "")
@@ -91,17 +89,14 @@ final class NotificationManager {
         triggerDateComponents.minute = minute
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDateComponents, repeats: true)
-        let request = UNNotificationRequest(identifier: person.id.uuidString, content: content, trigger: trigger)
+        let request = UNNotificationRequest(identifier: secondAlertIdentifier, content: content, trigger: trigger)
         
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                print("sa Error adding notification: \(error)")
-            }
-        }
+        UNUserNotificationCenter.current().add(request)
     }
     
     func removeNotifications(for person: Personn) {
-        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [person.id.uuidString])
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["firstAlert-" + person.id.uuidString])
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["secondAlert-" + person.id.uuidString])
     }
     
     func updateFirstAlertNotifications(for people: [Personn], reminder: Reminder, hour: Int, minute: Int) {
@@ -111,9 +106,8 @@ final class NotificationManager {
     }
     
     func updateSecondAlertNotifications(for people: [Personn], hour: Int, minute: Int) {
-        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
-        
         for person in people {
+            UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["secondAlert-" + person.id.uuidString])
             scheduleSecondAlertNotification(for: person, hour: hour, minute: minute)
         }
     }
